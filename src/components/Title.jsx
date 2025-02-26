beecd069159737ef780014a70945f251b18fefcf
Jensone <hello@jensone.com>
2025-02-26 13:34:10 +0100

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ export default function Title() {
     return (
     <header className="p-4 text-center">
         <h1 className="text-5xl lg:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-teal-500 to-cyan-600 shadow-2xl">
-            Hayabusa Rodsters TV
+            Hayabusa Roadsters TV
         </h1>
         <p className="text-lg mt-4">
             Une Hayabusa totalement <span className="font-medium text-cyan-200">UNIQUE</span>, que vous pourrez voir, toucher, et surtout <span className="font-medium text-cyan-200">GAGNER</span> !!
